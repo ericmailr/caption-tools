18f65ecbcbc8a665ad7377795f6bf45f49ec5c86
Eric <ericmailr@gmail.com>
2021-10-09 17:54:26 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,147 @@
+const submitScheduleButton = document.getElementById("submitSchedule");
+const rawScheduleInput = document.getElementById("rawSchedule");
+const result = document.getElementById("result");
+const submitRosterButton = document.getElementById("submitRoster");
+const rawRosterInput = document.getElementById("rawRoster");
+const rosterResult = document.getElementById("rosterResult");
+
+const prepTime = 15;
+
+let rosterOutput = "";
+const getRoster = () => {
+  let rawRoster = rawRosterInput.value;
+  let inputArray = rawRoster.split("\n");
+  console.log(inputArray);
+  inputArray.forEach((fullName) => {
+    if (/\S/.test(fullName)) {
+      let names = fullName.split(" ");
+      let lastName = names.at(-1);
+      console.log(fullName);
+      console.log(fullName + "\\" + fullName + " praim");
+      console.log(lastName);
+      console.log(lastName + "\\" + lastName + " praim");
+      rosterOutput += `${fullName}\n${fullName}\\${fullName} praim\n${lastName}\n${lastName}\\${lastName} praim\n`;
+    }
+  });
+  rosterResult.innerHTML = rosterOutput;
+};
+
+submitRosterButton.addEventListener("click", getRoster);
+
+Date.prototype.addDays = function (days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
+var date = new Date();
+
+const formatDate = (dateObject) => {
+  let month = dateObject.getMonth() + 1;
+  let day = dateObject.getDate();
+  let year = dateObject.getFullYear();
+  return `${month}/${day}/${year}`;
+};
+
+const getCSV = () => {
+  const rawSchedule = rawScheduleInput.value;
+  if (rawSchedule == "") {
+    let dates = rawSchedule.match(/((\d\d)|(\d))\/((\d\d)|(\d))\/20\d\d/g);
+    let times = rawSchedule.match(/((\d\d)|(\d)):\d\d(A|P)M/g);
+    let descriptions = rawSchedule.match(/Description: .*/g);
+
+    if (descriptions) {
+      descriptions = descriptions.map((d) => d.substring(13, d.length));
+    }
+
+    const militarizeTime = (time) => {
+      let hour = parseInt(time.substring(0, 2));
+      let minutes = time.substring(3, 5);
+      if (time.charAt(time.length - 2) === "A") {
+        return hour == 12 ? "00" + ":" + minutes : time.substring(0, 5);
+      } else {
+        let newHour = hour < 12 ? hour + 12 : 12;
+        let newTime = newHour + ":" + time.substring(3, 5);
+        return newTime;
+      }
+    };
+
+    dates.forEach((date, i) => {
+      dates[i] = new Date(dates[i]);
+    });
+
+    times.forEach((t, i) => {
+      times[i] = militarizeTime(t);
+    });
+
+    let endDates = JSON.parse(JSON.stringify(dates));
+
+    times.forEach((t, i) => {
+      // add prep time
+      if (i % 2 === 0) {
+        let hour = parseInt(t.substring(0, 2));
+        let minutes = parseInt(t.substring(3, 5));
+        if (minutes < prepTime) {
+          let newHour = hour - 1;
+          if (hour == 0) {
+            newHour = 23;
+            let dayOfMonth = parseInt(dates[i / 2].substring(3, 5)) - 1;
+            dates[i / 2] =
+              dates[i / 2].substring(0, 3) +
+              dayOfMonth +
+              dates[i / 2].substring(
+                dates[i / 2].length - 5,
+                dates[i / 2].length
+              );
+          }
+          if (newHour < 10) {
+            newHour = "0" + newHour;
+          }
+          let newMinutes = (60 - (prepTime - minutes)) % 60;
+          if (newMinutes < 10) {
+            newMinutes = "0" + newMinutes;
+          }
+          times[i] = newHour + ":" + newMinutes;
+        } else {
+          let newMinutes = minutes - prepTime;
+          if (newMinutes < 10) {
+            newMinutes = "0" + newMinutes;
+          }
+          if (hour < 10) {
+            hour = "0" + hour;
+          }
+          times[i] = hour + ":" + newMinutes;
+        }
+      }
+
+      if (i % 2 == 1) {
+        if (times[i].substring(0, 2) == "00") {
+          let endDate = new Date(dates[(i - 1) / 2]);
+          endDate.setDate(endDate.getDate() + 1);
+          endDates[(i - 1) / 2] = formatDate(endDate);
+        }
+      }
+    });
+  }
+
+  dates.forEach((date, i) => {
+    dates[i] = formatDate(new Date(dates[i]));
+    endDates[i] = formatDate(new Date(endDates[i]));
+  });
+
+  let csvContent = "";
+  csvContent +=
+    "Subject, Start Date, Start Time, End Date, End Time, All Day Event, Description, Private,\n";
+
+  dates.forEach((date, i) => {
+    csvContent += `Work, ${dates[i]}, ${times[2 * i]}, ${endDates[i]}, ${
+      times[2 * i + 1]
+    }, false, ${descriptions?.[i]}, true,\n`;
+  });
+  console.log(csvContent);
+  var encodedUri = encodeURI("data:text/csv;charset=utf-8,\n" + csvContent);
+  window.open(encodedUri);
+  result.innerHTML = csvContent;
+};
+
+submitScheduleButton.addEventListener("click", getCSV);
